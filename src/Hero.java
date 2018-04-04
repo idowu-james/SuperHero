a7f5f0eed912addd520fb1748e6c254223d3eba1
ID <idowu_james@rocketmail.com>
2018-04-04 03:15:51 +0100

    Checking if committed

---
+++ src/Hero.java
@@ -0,0 +1,39 @@
+public class Hero {
+
+
+
+  private int Strength;
+  private int speed;
+  private int health;
+  private String name;
+
+  //Getter Section ++++++++++++++++++
+  public int getStrength() {
+    return Strength;
+  }
+
+  public int getSpeed() {
+    return speed;
+  }
+
+  public int getHealth() {
+    return health;
+  }
+
+  //Constructor ++++++++++++++++++++++
+  public Hero(String name) {
+    this.name = name;
+  }
+
+  private void getAbilities(){
+
+  }
+
+   public void fight(){
+
+   }
+
+   public void run(){
+
+   }
+}
